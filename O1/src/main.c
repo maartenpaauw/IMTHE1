7bddd130513e74a47a4b216413b8853a070dcc29
Maarten Paauw <maartenpaauw@gmail.com>
2018-02-08 14:15:42 +0100

    chore: started with base exercise folder

---
+++ O1/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht 1
+ * 
+ * Maarten Paauw <s1094220@student.hsleiden.nl>
+ * s1094220
+ * INF3C
+ */
+`
+#include <avr/io.h>
+
+int main(void)
+{
+    while (1)
+    {
+    }
+
+    return 0;
+}
