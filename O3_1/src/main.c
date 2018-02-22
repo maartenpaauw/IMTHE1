133f34b8fa5a1dc64456ae4f8bb8dee4b06d50be
Maarten Paauw <maartenpaauw@gmail.com>
2018-02-22 16:47:43 +0100

    chore: started with exercise 3.1

--- O3_1/src/main.c
+++ O3_1/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Opdracht 0 - X
+ * Opdracht 3.1 - ADC Baby!
  * 
  * Maarten Paauw <s1094220@student.hsleiden.nl>
  * s1094220
@@ -8,13 +8,103 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
+
+// Genereer een random nummer tussen 1 en 6.
+int dobbel () {
+    return (rand() % 6) + 1;
+}
+
+// Regel de pinnen op de C bank.
+void handleC (int number) {
+    PORTC =~ number;
+}
+
+// Regel de pinnen op de D bank.
+void handleD (int number) {
+
+    // Door de RX en TX pinnen begin ik pas op pin D2.
+    // Daarom wordt er even 2x naar links gebitshift.
+    PORTD = (number << 2);
+}
+
+// Zet alle poorten uit.
+void clear () {
+    PORTC = 0b0000000;
+    PORTD = 0b0000000;
+}
+
+void dot (int number) {
+    number = number - 1;
+
+    int dots[9][2] = {
+        { 0b00000011, 0b00000011 },     // LINKS    BOVEN
+        { 0b00001100, 0b00000011 },     // MIDDEN   BOVEN
+        { 0b00110000, 0b00000011 },     // RECHTS   BOVEN
+        { 0b00000011, 0b00001100 },     // LINKS    MIDDEN
+        { 0b00001100, 0b00001100 },     // MIDDEN   MIDDEN
+        { 0b00110000, 0b00001100 },     // RECHTS   MIDDEN
+        { 0b00000011, 0b00110000 },     // LINKS    ONDER
+        { 0b00001100, 0b00110000 },     // MIDDEN   ONDER
+        { 0b00110000, 0b00110000 }      // RECHTS   ONDER
+    };
+
+    // Zet de C pinnen aan.
+    handleC(dots[number][0]);
+
+    // Zet de D pinnen aan.
+    handleD(dots[number][1]);
+}
+
+void side (int number) {
+
+    // Alle zijdes van een dobbelsteen met welke ogen er nodig zijn.
+    int sides[6][6] = {
+        { 5 },                  // 1
+        { 1, 9 },               // 2
+        { 1, 5, 9 },            // 3
+        { 1, 3, 7, 9},          // 4
+        { 1, 3, 5, 7, 9},       // 5
+        { 1, 3, 4, 6, 7, 9 }    // 6
+    };
+
+    // Loop door de ogen heen.
+    for (int i = 0; i < number; i++)
+    {
+        // Zet alle pinnen uit.
+        clear();
+
+        // Zet de dot aan.
+        dot(sides[number - 1][i]);
+
+        // Wacht voor 1 miliseconden.
+        _delay_ms(1);
+    }
+}
 
 int main(void)
 {
+    // Zet de C pinnen op output.
+    DDRC = 0b00111111;
+
+    // Zet de D pinnen op output.
+    DDRD = 0b11111100;
+
 
     while (1)
     {
+        // Genereer een random getal.
+        int res = dobbel();
 
+        for (int i = 0; i < 1000; i++) {
+            side(res);
+        }
+
+        // Zet alle pinnen uit.
+        clear();
+
+        // Pauzeer van 1 seconden.
+        _delay_ms(1000);
     }
 
     return 0;
