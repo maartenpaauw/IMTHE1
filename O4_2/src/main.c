37156bdf4d8f882b14b4cb6f67fc467e0ad583ed
Maarten Paauw <maartenpaauw@gmail.com>
2018-03-19 14:03:27 +0100

    chore: cant use usart

--- O4_2/src/main.c
+++ O4_2/src/main.c
@@ -5,11 +5,45 @@
  * s1094220
  * INF3C
  */
+#define BAUD 9600
 
 #include <avr/io.h>
-#include <USART.h>
 #include <util/setbaud.h>
 #include <util/delay.h>
+
+// Uit het bestand USART.
+void initUSART(void) {
+  UBRR0H = UBRRH_VALUE;
+  UBRR0L = UBRRL_VALUE;
+#if USE_2X
+  UCSR0A |= (1 << U2X0);
+#else
+  UCSR0A &= ~(1 << U2X0);
+#endif
+  UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+// Uit het bestand USART.
+uint8_t receiveByte(void) {
+  loop_until_bit_is_set(UCSR0A, RXC0);
+  return UDR0;
+}
+
+// Uit het bestand USART.
+void transmitByte(uint8_t data) {
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = data;
+}
+
+// Uit het bestand USART.
+void printString(const char myString[]) {
+  uint8_t i = 0;
+  while (myString[i]) {
+    transmitByte(myString[i]);
+    i++;
+  }
+}
 
 // Main functie.
 int main (void)
