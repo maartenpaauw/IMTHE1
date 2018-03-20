1c4676580dc04c6f9da50be0792bd369e8ed1b30
Maarten Paauw <maartenpaauw@gmail.com>
2018-03-20 16:33:28 +0100

    chore: exercise 4.3 code

--- O4_3/src/main.c
+++ O4_3/src/main.c
@@ -9,23 +9,37 @@
 // Baud rate.
 #define BAUD 9600
 
+// LED pinnen.
+#define LED_RED PB1
+#define LED_GREEN PB2
+#define LED_BLUE PB3
+
+// Includes.
 #include <avr/io.h>
 #include <util/setbaud.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+
+// Count.
+int count = 0;
+
+// Valiabele voor de kleur code (standaard rood).
+char color = 'r';
 
 // Uit het bestand USART.
 void initUSART(void)
 {
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
-    #if USE_2X
+
+     #if USE_2X
         UCSR0A |= (1 << U2X0);
     #else
         UCSR0A &= ~(1 << U2X0);
     #endif
-        UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-        UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+
+    // RXCIE0 zorgt ervoor dat de interrupt triggert op character.
+    UCSR0B = (1 << RXCIE0) | (1 << TXEN0) | (1 << RXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 // Uit het bestand USART.
@@ -60,7 +74,6 @@ void initTimerOverflow()
 
     // Timer instellingen.
     TCCR0B |= (1 << CS02) | (1 << CS00);
-    sei();
 }
 
 void intPWMTimerLED()
@@ -80,44 +93,110 @@ void intPWMTimerLED()
     TCCR2A |= (1 << COM2A1);
 }
 
-// LED pinnen.
-#define LED_RED PB1
-#define LED_GREEN PB2
-#define LED_BLUE PB3
+// Verander de led van kleur.
+void changeColor(int r, int g, int b) {
 
-// Count.
-int count = 0;
+    // Verander de rode led.
+    OCR1A = ~r;
 
-// Valiabele voor de kleur code.
-char color;
+    // Verander de groene led.
+    OCR1B = ~g;
+
+    // Verander de blauwe led.
+    OCR2A = ~b;
+}
+
+// Zet de kleur en stuur een bericht
+void setColorAndMessage(char c, char message[]) {
+       
+        // laat de gebruiker weten welke kleur het wordt.
+        printString(message);
+
+        // Reset de count.
+        count = 0;
+
+        // Sla het op in een variabele.
+        color = c;
+}
+
+// Timer overflow interrupt.
+ISR (TIMER0_OVF_vect)
+{
+    // Tel de count op, voor de brightness.
+    count++;
+
+    // Switch om te kijken om welke kleur het gaat.
+    switch(color)
+    {
+        // De kleur rood.
+        case 'r':
+
+            // Verander de led kleur.
+            changeColor(count, 0, 0);
+
+            // Break de case.
+            break;
+        
+        // De kleur groen.
+        case 'g':
+
+            // Verander de led kleur.
+            changeColor(0, count, 0);
+
+            // Break de case.
+            break;
+
+        // De kleur blauw.
+        case 'b':
+
+            // Verander de led kleur.
+            changeColor(0, 0, count);
+
+            // Break de case.
+            break;
+    }
+}
 
 // Als er geschreven wordt naar de seriele verbinding.
 ISR (USART_RX_vect)
 {
-    // Verkrijg de kleur vanuit de serial.
-    color = UDR0;
-
     // Kijk welke kleur het is.
-    switch (color)
+    switch (UDR0)
     {
     // Als het rood is.
     case 'r':
-        printString("De kleur wordt rood.\n");
+
+        // Zet de kleur en laat de gebruiker weten dat de kleur rood wordt.
+        setColorAndMessage('r', "De kleur wordt rood.\n");
+
+        // Break de case.
         break;
     
     // Als het groen is.
     case 'g':
-        printString("De kleur wordt groen.\n");
+
+        // Zet de kleur en laat de gebruiker weten dat de kleur rood wordt.
+        setColorAndMessage('g', "De kleur wordt groen.\n");
+
+        // Break de case.
         break;
 
     // Als het blauw is.
     case 'b':
-        printString("De kleur wordt blauw.\n");
+
+        // Zet de kleur en laat de gebruiker weten dat de kleur rood wordt.
+        setColorAndMessage('b', "De kleur wordt blauw.\n");
+
+        // Break de case.
         break;
 
     // De standaard switch case.
     default:
+
+        // Meld aan de gebruiker dat er een toets ingedrukt moet worden.
         printString("Toets R, G of B in.\n");
+
+        // Break de case.
         break;
     }
 }
@@ -136,6 +215,9 @@ int main(void)
 
     // Initialiseer de led timer.
     intPWMTimerLED();
+
+    // Zet de interrupts aan.
+    sei();
 
     // Loop voor altijd.
     while (1)
