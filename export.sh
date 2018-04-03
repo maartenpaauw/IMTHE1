20f95ce0f58e3da3c5bbc583f81af88cbd8d18b2
Maarten Paauw <maartenpaauw@gmail.com>
2018-04-03 14:36:43 +0200

    chore: export script

--- export.sh
+++ export.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Creating exported folder"
-mkdir -p "exported"
+echo "Creating exported folder";
+mkdir -p "exported";
 
-echo "Creating folder exported/word"
-mkdir -p "exported/word"
-echo "Exporting README.md in .docx format"
+echo "Creating folder exported/word";
+mkdir -p "exported/word";
+echo "Exporting README.md in .docx format";
 
 pandoc EI/README.md -s -o exported/word/EI.docx &&
 pandoc O1/README.md -s -o exported/word/O1.docx &&
@@ -17,6 +17,6 @@ pandoc O4_1/README.md -s -o exported/word/O4_1.docx &&
 pandoc O4_2/README.md -s -o exported/word/O4_2.docx &&
 pandoc O4_3/README.md -s -o exported/word/O4_3.docx &&
 pandoc O5_1/README.md -s -o exported/word/O5_1.docx &&
-pandoc O5_2/README.md -s -o exported/word/O5_2.docx
+pandoc O5_2/README.md -s -o exported/word/O5_2.docx;
 
-echo "done"
+echo "done";
